1bc1b640b9d58b7e494d715d4012d4108f39a944
Apurv099 <apurvgurav.619@mail.com>
2025-11-11 08:43:09 +0530

    force Newsletter extension

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,8 @@ import Hero from '../components/Hero'
 import LatestCollection from '../components/LatestCollection'
 import BestSeller from '../components/BestSeller'
 import OurPolicy from "../components/OurPolicy"
-import Newsletter from "../components/Newsletter"
+import Newsletter from "../components/Newsletter.jsx"
+
 
 
 const Home = () => {
